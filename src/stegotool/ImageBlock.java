8c8c34fd05b4f524e1b19b10dbec6bb01d49124b
holzmanj <jesseholzman96@gmail.com>
2018-04-12 13:39:31 -0400

    Fixed constructor and implemented getImage method.

--- src/stegotool/ImageBlock.java
+++ src/stegotool/ImageBlock.java
@@ -10,7 +10,7 @@ import java.awt.image.BufferedImage;
  * @author jesse
  */
 public class ImageBlock {
-    byte[][] bitPlanes;
+    byte[][][] bitPlanes;   // indices are ordered [color][bitlevel][row]
     
     /**
      * Constructor takes 8x8 subimage and stores it as an array of bit
@@ -31,32 +31,62 @@ public class ImageBlock {
         int numColorChannels = img.getColorModel().getNumColorComponents();
         switch (numColorChannels) {
             case 3:
-                bitPlanes = new byte[3][8];
+                bitPlanes = new byte[3][8][8];
                 break;
             case 1:
-                bitPlanes = new byte[1][8];
+                bitPlanes = new byte[1][8][8];
                 break;
             default:
                 throw new RuntimeException("Invalid image color format.");
         }
         
         // Convert subimage to bit planes
-        byte b, mask;
+        byte b;
         int c;
         for(int color = 0; color < numColorChannels; color++) {
             for(int bitLevel = 0; bitLevel < 8; bitLevel++) {
-                mask = (byte) (0b01 << bitLevel);
                 for(int row = 0; row < 8; row++) {
                     b = 0;
                     for(int col = 0; col < 8; col++) {
+                        b = (byte) (b << 1);
                         c = img.getRGB(col, row);
                         
-                        b |= (c >> (8 * color)) & mask;
-                        b = (byte) (b << 1);
+                        b |= (c >> (bitLevel + (8 * color))) & 1;
                     }
-                    bitPlanes[color][row] = b;
+                    bitPlanes[color][bitLevel][row] = b;
                 }
             }
         }
+    }
+    
+    /**
+     * Converts the bit planes back into a BufferedImage.
+     * @return 8x8 image
+     */
+    public BufferedImage getImage() {
+        BufferedImage img;
+        int mask, b, c;
+        // initialize output image to 8x8 black square
+        if(bitPlanes.length == 3)
+            img = new BufferedImage(8, 8, BufferedImage.TYPE_INT_RGB);
+        else
+            img = new BufferedImage(8, 8, BufferedImage.TYPE_BYTE_GRAY);
+
+        // iterate through bit planes and reconstruct image
+        for(int color = 0; color < bitPlanes.length; color++) {
+            for(int bitLevel = 0; bitLevel < 8; bitLevel++) {
+                mask = 1 << bitLevel;
+                for(int row = 0; row < 8; row++) {
+                    for(int col = 0; col < 8; col++) {
+                        c = img.getRGB(col, row);
+                        b = bitPlanes[color][bitLevel][row];
+                        c |= ((b << ((8 * color) + bitLevel)) >> 7 - col)       // shift bit to appropriate index in RGB int
+                                & (mask << 8 * color);                          // mask specific bit to RGB int
+                        img.setRGB(col, row, c);
+                    }
+                }
+            }
+        }
+        return img;
     }
 }
