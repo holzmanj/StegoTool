f6dcbdef009e6cec8c638ad243b99a7b0775a054
holzmanj <jesseholzman96@gmail.com>
2018-04-16 16:16:30 -0400

    Implemented callable thread for insertion.

--- src/stegotool/LSBMultithreader.java
+++ src/stegotool/LSBMultithreader.java
@@ -4,16 +4,30 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.concurrent.Callable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
- * 
+ * Divides an image up into sections and creates an LSB thread for each
+ * image section.
  * @author jesse
  */
 public class LSBMultithreader implements StegoTechnique {
+    final int BITS_PER_BYTE = 2;
+    final int THREAD_COUNT;
+    
+    public LSBMultithreader(int threads) {
+        THREAD_COUNT = threads;
+    }
 
     @Override
     public int getImageCapacity(BufferedImage img) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int colorChannels = img.getRaster().getNumDataElements();
+        int width = img.getWidth();
+        int height = img.getHeight();
+        
+        return (width * height * colorChannels * BITS_PER_BYTE) / 8;
     }
 
     @Override
@@ -24,6 +38,29 @@ public class LSBMultithreader implements StegoTechnique {
     @Override
     public void extractFile(BufferedImage img, File outputFile) throws FileNotFoundException, IOException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
+
+class LSBInsertThread implements Callable<BufferedImage> {
+    LSBTechnique t;
+    BufferedImage imgInput, imgOutput;
+    InputFileBitStream bitStream;
+    
+    public LSBInsertThread(BufferedImage img, InputFileBitStream stream) {
+        t = new LSBTechnique();
+        imgInput = img;
+        bitStream = stream;
+    }
+
+    @Override
+    public BufferedImage call() {
+        try {
+            imgOutput = t.insertRawData(bitStream, imgInput);
+        } catch (IOException ex) {
+            Logger.getLogger(LSBInsertThread.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return imgOutput;
     }
     
 }
