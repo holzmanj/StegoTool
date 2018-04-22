12a361a508986ba3e64ef870c355a63302891ef7
holzmanj <jesseholzman96@gmail.com>
2018-04-22 18:55:25 -0400

    Implemented method for calculating bit plane complexity.

--- src/stegotool/BPCSTechnique.java
+++ src/stegotool/BPCSTechnique.java
@@ -18,11 +18,34 @@ public class BPCSTechnique implements StegoTechnique {
     private final double COMPLEXITY_THRESHOLD = 0.4;
     
     public BPCSTechnique() {
-    }        
+    }
+
+    
+    private double calculateComplexity(byte[] bitPlane) {
+        int sum = 0;
+        
+        // count horizontal bit flips
+        for(int row = 0; row < 8; row++) {
+            for(int col = 0; col < 7; col++) {
+                sum += ((bitPlane[row] >> col) ^ (bitPlane[row] >> (col + 1))) & 1;
+            }
+        }
+        
+        // count vertical bit flips
+        for(int col = 0; col < 8; col++) {
+            for(int row = 0; row < 7; row++) {
+                sum += ((bitPlane[row] >> col) ^ (bitPlane[row + 1] >> col)) & 1;
+            }
+        }
+        
+        System.out.println("sum: " + sum);
+        
+        return (double) sum / 112.0;
+    }
 
     @Override
     public int getImageCapacity(BufferedImage img) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return 0;
     }
 
     @Override
