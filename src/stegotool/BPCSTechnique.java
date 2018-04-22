cb5772e052fb9bbcc135f123cb0d748aa2f49b17
holzmanj <jesseholzman96@gmail.com>
2018-04-22 19:02:35 -0400

    Implemented method to convert image to ImageBlock array

--- src/stegotool/BPCSTechnique.java
+++ src/stegotool/BPCSTechnique.java
@@ -20,26 +20,35 @@ public class BPCSTechnique implements StegoTechnique {
     public BPCSTechnique() {
     }
 
+    private ImageBlock[][] imageToBlocks(BufferedImage img) {
+        int xBlocks = Math.floorDiv(img.getWidth(), 8);
+        int yBlocks = Math.floorDiv(img.getHeight(), 8);
+        ImageBlock[][] imgBlocks = new ImageBlock[xBlocks][yBlocks];
+        
+        for(int x = 0; x < xBlocks; x++) {
+            for(int y = 0; y < yBlocks; y++) {
+                imgBlocks[x][y] = new ImageBlock(
+                        img.getSubimage(x * 8, y * 8, 8, 8));
+            }
+        }
+        
+        return imgBlocks;
+    }
     
     private double calculateComplexity(byte[] bitPlane) {
         int sum = 0;
-        
         // count horizontal bit flips
         for(int row = 0; row < 8; row++) {
             for(int col = 0; col < 7; col++) {
                 sum += ((bitPlane[row] >> col) ^ (bitPlane[row] >> (col + 1))) & 1;
             }
         }
-        
         // count vertical bit flips
         for(int col = 0; col < 8; col++) {
             for(int row = 0; row < 7; row++) {
                 sum += ((bitPlane[row] >> col) ^ (bitPlane[row + 1] >> col)) & 1;
             }
         }
-        
-        System.out.println("sum: " + sum);
-        
         return (double) sum / 112.0;
     }
 
